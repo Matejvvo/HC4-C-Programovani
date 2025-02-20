2d317fcd3ae18a44704f4814f9992ce17bca7b86
Matej Volkmer <matejvvo@icloud.com>
2025-02-20 18:13:00 +0100

    krouzek 18

---
+++ Krouzek 18/hello_world.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+int main() {
+    printf("Hello, World.\n");
+    printf("gjsdfjkhgdsfjgh");
+    printf("gjsdfjkhgdsfjgh");
+    printf("gjsdfjkhgdsfjgh");
+    printf("gjsdfjkhgdsfjgh");
+
+    return 0;
+}
---
+++ Krouzek 18/soubory.c
@@ -0,0 +1,31 @@
+#include "stdio.h"
+#include <stdlib.h>
+
+int main() {
+    FILE *file;  // Ukazatel na soubor
+
+    // *** ZÁPIS DO SOUBORU ***
+    file = fopen("soubor.txt", "rw"); // Otevření souboru pro čtení a zápis
+    // if (file == NULL) {
+    //     perror("Chyba při otevírání souboru pro zápis");
+    //     return 1;
+    // }
+
+    fprintf(file, "Ahoj, toto je text zapsaný do souboru cislo %d!\n", 56);
+    fprintf(file, "Druhý řádek textu.\n");
+
+    fclose(file); // Zavření souboru
+
+    file = fopen("soubor.txt", "r"); // Otevření souboru pro čtení a zápis
+
+    // // *** ČTENÍ ZE SOUBORU ***
+    printf("Obsah souboru output.txt:\n");
+    char buffer[256];  // Pole pro čtení řádků
+    while (fgets(buffer, sizeof(buffer), file)) 
+    { 
+        printf("%s", buffer); // Výpis obsahu souboru na obrazovku
+    }
+
+    fclose(file); // Zavření souboru
+    return 0;
+}
