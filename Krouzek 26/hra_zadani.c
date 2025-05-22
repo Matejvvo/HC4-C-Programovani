f51e46a54182d13aba2dd7a02990fd1d19142fab
Matej Volkmer <matejvvo@icloud.com>
2025-05-22 16:59:42 +0200

    kr27: hra

---
+++ Krouzek 26/hra_zadani.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// bit.ly/hc4prog <- stahnout sablonu
+
+char mapa[10][10 + 1] = {
+    "##########",
+    "#@       #",
+    "#  ####  #",
+    "#     #  #",
+    "#  #     #",
+    "#  #  ####",
+    "#     #  #",
+    "### #    #",
+    "#      X #",
+    "##########"
+};
+
+int y = 1, x = 1;
+
+int main() {
+
+
+
+    return 0;
+}
